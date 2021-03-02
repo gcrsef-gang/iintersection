1d3440c7b962ad88555ce8457de6ea915dba2c57
k-vernooy <kai.a.vernooy@gmail.com>
2021-03-01 20:36:06 -0500

    Add intersection test without parser

--- tests/libiintersection_tests.cpp
+++ tests/libiintersection_tests.cpp
@@ -5,24 +5,29 @@ using namespace std;
 using namespace ii;
 
 
+Intersection getIntersection() {
+    IntersectionNode* n1 = GLOBALDATA->createIntersectionNode(Point3d(-1, 0, 0), JUNCTIONTYPES::UNREGULATED);
+    IntersectionNode* n2 = GLOBALDATA->createIntersectionNode(Point3d(1, 0, 0), JUNCTIONTYPES::UNREGULATED);
+    IntersectionNode* n3 = GLOBALDATA->createIntersectionNode(Point3d(0, 0, 0), JUNCTIONTYPES::UNREGULATED);
+    IntersectionNode* n4 = GLOBALDATA->createIntersectionNode(Point3d(0, 1, 0), JUNCTIONTYPES::UNREGULATED);
+    IntersectionNode* n5 = GLOBALDATA->createIntersectionNode(Point3d(0, -1, 0), JUNCTIONTYPES::UNREGULATED);
+
+
+    IntersectionEdge e1 = IntersectionEdge(n1, n3, BezierCurve(n1, n3, {}), 1, 45, 1);
+    IntersectionEdge e2 = IntersectionEdge(n3, n2, BezierCurve(n3, n2, {}), 1, 45, 2);
+    IntersectionEdge e3 = IntersectionEdge(n4, n3, BezierCurve(n4, n3, {}), 1, 45, 3);
+    IntersectionEdge e4 = IntersectionEdge(n3, n5, BezierCurve(n3, n5, {}), 1, 45, 4);
+    
+    
+    IntersectionRoute r1 = IntersectionRoute({n1, n3, n2}, {e1, e2});
+    IntersectionRoute r2 = IntersectionRoute({n4, n3, n5}, {e3, e4});
+
+    return Intersection({r1, r2});
+}
+
+
 int main() {
-    // IntersectionScenario int_("monte-carlo.xml");
-    // for (int i = 0; i < int_.getNodes().size(); i++) {
-    //     cout << int_.getNodes()[i].getID() << endl; 
-    // }
-    Point3d p1(500, 1000, 500);
-    Point3d p2(3500, 1500, 0);
-    Point3d h1(1500, 1700, 800);
-    Point3d h2(2500, 1900, -200);
-
-    IntersectionNode* n1 = GLOBALDATA->createIntersectionNode(p1, JUNCTIONTYPES::UNREGULATED);
-    IntersectionNode* n2 = GLOBALDATA->createIntersectionNode(p2, JUNCTIONTYPES::UNREGULATED);
-
-    BezierCurve curve(n1, n2, {h1, h2});
-
-    std::vector<Point3d> points = curve.rasterize(200);
-
-    for (Point3d p : points) {
-        cout << p.x() << "\t" << p.y() << "\t" << p.z() << endl;
-    }
+    Intersection int_ = getIntersection();
+    int_.simulate(BACKENDS::SUMO);
+    int_.getMetric(METRICS::EFFICIENCY);
 }
